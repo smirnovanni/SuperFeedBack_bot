539f59b7656ee2a6b918048e709359e1095fd9fb
Anton <forsaken@hu2.ru>
2019-04-08 22:56:49 +0300

    путь к файлам сертификатов

--- source/config.py
+++ source/config.py
@@ -8,8 +8,8 @@ WEBHOOK_HOST = 'smirnovan.ru'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = './source/cert/webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './source/cert/webhook_pkey.pem'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
