a13ef4a8d930f98d0f5875b57d3913b23d67d51f
Anton <forsaken@hu2.ru>
2019-04-08 23:34:20 +0300

    callback with keyboard

--- source/bot.py
+++ source/bot.py
@@ -2,7 +2,7 @@ import logging
 
 import telebot
 
-from source.config import TOKEN
+from source.config import TOKEN, ADMIN
 from source.log import log
 from source.variables import *
 
@@ -21,9 +21,21 @@ def initial_bot(use_logging=True, level_name='DEBUG'):
         bot.send_message(message.from_user.id, help_mess)
         logger.info(f"It's start handler. Message from {message.from_user.id}")
 
+    @bot.callback_query_handler(func=lambda call: call.data in ['approve'])
+    def callback_handler(call):
+        try:
+            bot.send_message(call.from_user.id, call.message.text)
+        except:
+            pass
+
     @bot.message_handler(content_types=['text'])
     def text_handler(message: telebot.types.Message):
-        bot.send_message(message.from_user.id, help_mess)
+
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.add(telebot.types.InlineKeyboardButton('Опубликовать', callback_data='approve'),
+                     telebot.types.InlineKeyboardButton('Отклонить', callback_data='refuse'))
+
+        bot.send_message(ADMIN, message.text, reply_markup=keyboard)
         logger.info(f"It's text handler. Message from {message.from_user.id}")
 
     if use_logging:
